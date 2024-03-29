42772116c6a7d849442821ba987199b84fd24a74
Nurwahid Muslim <112595135+nurwahidmuslim@users.noreply.github.com>
2024-03-29 17:39:42 +0700

    Update daftar.php

--- daftar.php
+++ daftar.php
@@ -17,22 +17,11 @@
     <form method="post">
         <h2>Daftar</h2>
         <p>Hi, Selamat Datang !</p>
-        <!-- Tampilkan pesan alert jika ada -->
         <?php
-        // Variabel untuk menyimpan pesan alert
         $alert_message = "";
 
-        // Periksa koneksi
-        $servername = "localhost";
-        $username = "root";
-        $password = ""; 
-        $dbname = "hotelwise";
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Koneksi gagal: " . $conn->connect_error);
-        }
+        include 'koneksi.php';
 
-        // Jika formulir disubmit
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $email = $_POST['email'];
             $namaL = $_POST['namaL'];
@@ -40,21 +29,18 @@
             $password = $_POST['password'];
             $konfPassword = $_POST['konf_password'];
 
-            // Periksa apakah semua bidang formulir telah diisi
             if (empty($email) || empty($namaL) || empty($namaP) || empty($password) || empty($konfPassword)) {
                 $alert_message = "Semua bidang harus diisi!";
             } else {
-                // Periksa apakah konfirmasi kata sandi sesuai
                 if ($password !== $konfPassword) {
                     $alert_message = "Konfirmasi kata sandi tidak cocok!";
                 } else {
-                    // Query untuk menyimpan data ke database
                     $sql = "INSERT INTO users (email, nama_lengkap, nama_panggilan, password)
                     VALUES ('$email', '$namaL', '$namaP', '$password')";
 
                     if ($conn->query($sql) === TRUE) {
-                        // Notifikasi berhasil
                         $alert_message = "Pendaftaran berhasil!";
+                        echo "<script>setTimeout(function(){ window.location.href = 'masuk.php'; }, 1000);</script>";
                     } else {
                         echo "Error: " . $sql . "<br>" . $conn->error;
                     }
@@ -62,7 +48,6 @@
             }
         }
 
-        // Tutup koneksi
         $conn->close();
 
         if (!empty($alert_message)) {
@@ -87,20 +72,18 @@
         </div><br>
         <button type="submit">Daftar</button><br><br><br><br>
         <p style="text-align: center;">Sudah punya akun?</p>
-        <a href="masuk.html">Masuk disini</a>
+        <a href="masuk.php">Masuk disini</a>
     </form>
 </div>
 
 <script>
-    // Ambil pesan alert
     var alertMessage = document.getElementById('alertMessage');
 
-    // Jika terdapat pesan alert, tampilkan dan hilangkan setelah ? detik
     if (alertMessage.innerHTML.trim() !== '') {
         alertMessage.style.display = 'block';
         setTimeout(function () {
             alertMessage.style.display = 'none';
-        }, 1000);
+        }, 2000);
     }
 
     function togglePassword(inputId) {
