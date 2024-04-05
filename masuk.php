1ce0c2d35ab5fab5bab6cdfe2ef814ae9b1eeae8
Nurwahid Muslim <112595135+nurwahidmuslim@users.noreply.github.com>
2024-04-06 01:06:13 +0700

    Update masuk.php

--- masuk.php
+++ masuk.php
@@ -27,59 +27,57 @@ $conn->close();
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Masuk</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-<nav class="navbar">
-    <div class="navbar-left">
-        <span class="navbar-brand"><span class="wise">Wise</span><span class="hotel">Hotel</span></span>
-    </div>
-</nav>
-<div class="masuk">
-    <form method="post">
-        <h2>Masuk</h2>
-        <p>Hi, Selamat Datang !</p>
-        <?php if ($alert_message): ?>
-            <div class="alert"><?php echo $alert_message; ?></div>
-        <?php endif; ?>
-        <label for="email">Email</label><br>
-        <input type="email" id="email" name="email" value="" placeholder="Masukan email"><br><br>
-        <label for="password">Sandi</label><br>
-        <div class="password-container">
-            <input type="password" id="password" name="password" value="" placeholder="Masukan kata sandi">
-            <img class="password-toggle" src="mata.png" alt="Show Password" onclick="togglePassword('password')">
-        </div><br>
-        <a href="#">Lupa sandi?</a><br>
-        <button type="submit">Masuk</button><br><br><br><br>
-    </form>
-</div>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Masuk</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="navbar-left">
+            <span class="navbar-brand">WiseHotel</span>
+        </div>
+        <div class="masuk">
+            <form method="post">
+                <h2>Masuk</h2>
+                <p>Hi, Selamat Datang !</p>
+                <?php if ($alert_message): ?>
+                    <div class="alert"><?php echo $alert_message; ?></div>
+                <?php endif; ?>
+                <label for="email">Email</label><br>
+                <input type="email" id="email" name="email" value="" placeholder="Masukan email"><br><br>
+                <label for="password">Sandi</label><br>
+                <div class="password-container">
+                    <input type="password" id="password" name="password" value="" placeholder="Masukan kata sandi">
+                    <img class="password-toggle" src="mata.svg" alt="Show Password" onclick="togglePassword('password')">
+                </div><br>
+                <a href="#">Lupa sandi?</a><br>
+                <button type="submit">Masuk</button><br><br><br><br>
+            </form>
+        </div>
 
-<script>
-    var alertMessage = document.querySelector('.alert');
+        <script>
+            var alertMessage = document.querySelector('.alert');
 
-    if (alertMessage && alertMessage.innerHTML.trim() !== '') {
-        alertMessage.style.display = 'block';
-        setTimeout(function () {
-            alertMessage.style.display = 'none';
-        }, 2000);
-    }
+            if (alertMessage && alertMessage.innerHTML.trim() !== '') {
+                alertMessage.style.display = 'block';
+                setTimeout(function () {
+                    alertMessage.style.display = 'none';
+                }, 2000);
+            }
 
-    function togglePassword(inputId) {
-        var passwordInput = document.getElementById(inputId);
-        var passwordToggle = document.querySelector('#' + inputId + ' + .password-toggle');
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text";
-            passwordToggle.src = "mata.png";
-        } else {
-            passwordInput.type = "password";
-            passwordToggle.src = "mata.png";
-        }
-    }
-</script>
+            function togglePassword(inputId) {
+                var passwordInput = document.getElementById(inputId);
+                var passwordToggle = document.querySelector('#' + inputId + ' + .password-toggle');
+                if (passwordInput.type === "password") {
+                    passwordInput.type = "text";
+                    passwordToggle.src = "mata.svg";
+                } else {
+                    passwordInput.type = "password";
+                    passwordToggle.src = "mata.svg";
+                }
+            }
+        </script>
 
-</body>
+    </body>
 </html>
