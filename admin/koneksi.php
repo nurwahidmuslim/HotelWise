a5c37fecaf56113075ba59d3184a4a2046a6f70c
Wayan Santie Arif <64580513+wyanns-404@users.noreply.github.com>
2024-05-21 17:08:38 +0700

    Update koneksi.php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost"; 
-    $username = "root";
-    $password = "";
-    $dbname = "hotelwise";
+    $servername = "sql12.freesqldatabase.com"; 
+    $username = "sql12708056";
+    $password = "MV99vXfbvB";
+    $dbname = "sql12708056";
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
         die("Koneksi gagal: " . $conn->connect_error);
